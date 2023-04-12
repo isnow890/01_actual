7df5ac8325ae5e476afaff7117e769e50261772c
isnow890 <isnow890@hotmail.com>
2023-04-12 21:30:21 +0900

    Dio로 Auth API 요청해보기

--- code_factory_intermediate/01_actual/lib/user/view/login_screen.dart
+++ code_factory_intermediate/01_actual/lib/user/view/login_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:actual/common/const/colors.dart';
 import 'package:actual/common/layout/default_layout.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 import '../../common/component/custom_text_form_field.dart';
@@ -9,6 +13,14 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final emulatorIp = '10.0.2.2:3000';
+    final simulatorIp = '127.0.0.1:3000';
+
+    //런타임에 어떤 운영체제에서 사용중인지 알수 있음.
+    final ip = Platform.isIOS ? simulatorIp : emulatorIp;
+
+    final dio = Dio();
+
     return DefaultLayout(
       //SingleChildScrollView를 선언하여 키보드가 튀어나와도 화면 에러 안나도록
       child: SingleChildScrollView(
@@ -25,7 +37,9 @@ class LoginScreen extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 _Title(),
-                const SizedBox(height: 16,),
+                const SizedBox(
+                  height: 16,
+                ),
                 _SubTitle(),
                 //사이즈 2/3
                 Image.asset(
@@ -37,24 +51,61 @@ class LoginScreen extends StatelessWidget {
                   hintText: '이메일을 입력해주세요.',
                   onChanged: (value) {},
                 ),
-                const SizedBox(height: 16,),
+                const SizedBox(
+                  height: 16,
+                ),
 
                 CustomTextFormField(
                   hintText: '비밀번호를 입력해주세요.',
                   obscureText: true,
                   onChanged: (value) {},
                 ),
-                const SizedBox(height: 16,),
-
+                const SizedBox(
+                  height: 16,
+                ),
+                //dio post 요청 보내기
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () async {
+                    //안드로이드 로컬 에뮬레이터 기준으로
+
+                    //ID:비밀번호
+                    final rawString = 'test@codefactory.ai:testtest';
+
+                    //base64 변환방법
+                    Codec<String, String> stringToBase64 = utf8.fuse(base64);
+                    String token = stringToBase64.encode(rawString);
+
+                    final resp = await dio.post(
+                      'http://$ip/auth/login',
+                      options: Options(
+                        headers: {
+                          'authorization': 'Basic $token',
+                        },
+                      ),
+                    );
+                    print(resp.data);
+                  },
                   child: Text('로그인'),
                   style: ElevatedButton.styleFrom(
                     primary: PRIMARY_COLOR,
                   ),
                 ),
                 TextButton(
-                  onPressed: () {},
+                  onPressed: () async{
+                    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3RAY29kZWZhY3RvcnkuYWkiLCJzdWIiOiJmNTViMzJkMi00ZDY4LTRjMWUtYTNjYS1kYTlkN2QwZDkyZTUiLCJ0eXBlIjoicmVmcmVzaCIsImlhdCI6MTY4MTMwMjMxNywiZXhwIjoxNjgxMzg4NzE3fQ.fuPxGyR2GaDG3B1o7WuuHkd1Gp6EjjdLcwk4nLLMP_I';
+
+                    final resp = await dio.post(
+                      'http://$ip/auth/token',
+                      options: Options(
+                        headers: {
+                          'authorization': 'Bearer $token',
+                        },
+                      ),
+                    );
+                    print(resp.data);
+
+
+                  },
                   child: Text('회원가입'),
                   style: TextButton.styleFrom(
                     foregroundColor: Colors.black,
