34ab6956d47eb4ba3d8ec331dd8f1a8d29679a1f
isnow890 <isnow890@hotmail.com>
2023-04-12 21:46:16 +0900

    간단한 로그인 시스템 만들어보기

--- code_factory_intermediate/01_actual/lib/user/view/login_screen.dart
+++ code_factory_intermediate/01_actual/lib/user/view/login_screen.dart
@@ -3,13 +3,22 @@ import 'dart:io';
 
 import 'package:actual/common/const/colors.dart';
 import 'package:actual/common/layout/default_layout.dart';
+import 'package:actual/common/view/root_tab.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 import '../../common/component/custom_text_form_field.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  String username = '';
+  String password = '';
 
   @override
   Widget build(BuildContext context) {
@@ -49,7 +58,9 @@ class LoginScreen extends StatelessWidget {
                 ),
                 CustomTextFormField(
                   hintText: '이메일을 입력해주세요.',
-                  onChanged: (value) {},
+                  onChanged: (value) {
+                    username = value;
+                  },
                 ),
                 const SizedBox(
                   height: 16,
@@ -58,7 +69,9 @@ class LoginScreen extends StatelessWidget {
                 CustomTextFormField(
                   hintText: '비밀번호를 입력해주세요.',
                   obscureText: true,
-                  onChanged: (value) {},
+                  onChanged: (value) {
+                    password = value;
+                  },
                 ),
                 const SizedBox(
                   height: 16,
@@ -66,10 +79,12 @@ class LoginScreen extends StatelessWidget {
                 //dio post 요청 보내기
                 ElevatedButton(
                   onPressed: () async {
-                    //안드로이드 로컬 에뮬레이터 기준으로
+                    //test@codefactory.ai
+                    //testtest
 
                     //ID:비밀번호
-                    final rawString = 'test@codefactory.ai:testtest';
+                    final rawString = '$username:$password';
+                    print(rawString);
 
                     //base64 변환방법
                     Codec<String, String> stringToBase64 = utf8.fuse(base64);
@@ -83,6 +98,12 @@ class LoginScreen extends StatelessWidget {
                         },
                       ),
                     );
+
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (_) => RootTab(),
+                      ),
+                    );
                     print(resp.data);
                   },
                   child: Text('로그인'),
@@ -91,8 +112,9 @@ class LoginScreen extends StatelessWidget {
                   ),
                 ),
                 TextButton(
-                  onPressed: () async{
-                    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3RAY29kZWZhY3RvcnkuYWkiLCJzdWIiOiJmNTViMzJkMi00ZDY4LTRjMWUtYTNjYS1kYTlkN2QwZDkyZTUiLCJ0eXBlIjoicmVmcmVzaCIsImlhdCI6MTY4MTMwMjMxNywiZXhwIjoxNjgxMzg4NzE3fQ.fuPxGyR2GaDG3B1o7WuuHkd1Gp6EjjdLcwk4nLLMP_I';
+                  onPressed: () async {
+                    const token =
+                        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3RAY29kZWZhY3RvcnkuYWkiLCJzdWIiOiJmNTViMzJkMi00ZDY4LTRjMWUtYTNjYS1kYTlkN2QwZDkyZTUiLCJ0eXBlIjoicmVmcmVzaCIsImlhdCI6MTY4MTMwMjMxNywiZXhwIjoxNjgxMzg4NzE3fQ.fuPxGyR2GaDG3B1o7WuuHkd1Gp6EjjdLcwk4nLLMP_I';
 
                     final resp = await dio.post(
                       'http://$ip/auth/token',
@@ -103,8 +125,6 @@ class LoginScreen extends StatelessWidget {
                       ),
                     );
                     print(resp.data);
-
-
                   },
                   child: Text('회원가입'),
                   style: TextButton.styleFrom(
