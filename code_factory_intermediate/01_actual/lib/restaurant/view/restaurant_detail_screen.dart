d666f3a74c0a2d61dfaff1f462d8966442b3bf8b
양찬우 <isnow890@hotmail.com>
2023-04-17 13:27:34 +0900

    ProductCard 작업하기

--- code_factory_intermediate/01_actual/lib/restaurant/view/restaurant_detail_screen.dart
+++ code_factory_intermediate/01_actual/lib/restaurant/view/restaurant_detail_screen.dart
@@ -2,6 +2,8 @@ import 'package:actual/common/layout/default_layout.dart';
 import 'package:actual/restaurant/component/restaurant_card.dart';
 import 'package:flutter/material.dart';
 
+import '../../product/component/product_card.dart';
+
 class RestaurantDetailScreen extends StatelessWidget {
   const RestaurantDetailScreen({Key? key}) : super(key: key);
 
@@ -20,7 +22,12 @@ class RestaurantDetailScreen extends StatelessWidget {
             ratings: 4.76,
             isDetail: true,
             detail: '맛있는 떡볶이',
-          )
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: ProductCard(),
+          ),
+
         ]));
   }
 }
