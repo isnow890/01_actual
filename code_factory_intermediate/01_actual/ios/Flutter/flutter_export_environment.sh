9acb6db81bd3b6c6be1145da2da9530e0e10f341
isnow890 <isnow890@hotmail.com>
2023-04-19 00:00:02 +0900

    flutter channel -> stable로 변경함.

--- code_factory_intermediate/01_actual/ios/Flutter/flutter_export_environment.sh
+++ code_factory_intermediate/01_actual/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\repo\code_factory\01_actual\code_factory_intermediate\01_actual"
+export "FLUTTER_APPLICATION_PATH=D:\repos\code_factory_intermediate\01_actual"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
