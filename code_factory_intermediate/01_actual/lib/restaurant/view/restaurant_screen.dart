79c00a9b265410bbf23007c711adde5056d04dbe
양찬우 <isnow890@hotmail.com>
2023-04-25 14:51:41 +0900

    FetchMore 로딩 UI 작업하기

--- code_factory_intermediate/01_actual/lib/restaurant/view/restaurant_screen.dart
+++ code_factory_intermediate/01_actual/lib/restaurant/view/restaurant_screen.dart
@@ -62,11 +62,24 @@ class _RestaurantScreenState extends ConsumerState<RestaurantScreen> {
 
     return ListView.separated(
       controller: controller,
-      itemCount: cp!.data.length,
+      itemCount: cp!.data.length + 1,
       separatorBuilder: (context, index) => SizedBox(
         height: 16.0,
       ),
       itemBuilder: (context, index) {
+        if (index == cp.data.length) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16.0,
+              vertical: 8.0,
+            ),
+            child: Center(
+                child: data is CursorPaginationFetchingMore
+                    ? CircularProgressIndicator()
+                    : Text('마지막 데이터입니다.')),
+          );
+        }
+
         //parsed
         final pItem = cp.data[index];
 
