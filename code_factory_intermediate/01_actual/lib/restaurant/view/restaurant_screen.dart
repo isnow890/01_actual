a908b58645e58caf4b7dd370c37397de186b32ef
양찬우 <isnow890@hotmail.com>
2023-04-25 14:46:48 +0900

    완성된 Pagination 로직 실행해보기

--- code_factory_intermediate/01_actual/lib/restaurant/view/restaurant_screen.dart
+++ code_factory_intermediate/01_actual/lib/restaurant/view/restaurant_screen.dart
@@ -1,6 +1,4 @@
-import 'package:actual/restaurant/model/restaurant_model.dart';
 import 'package:actual/restaurant/provider/restaurant_provider.dart';
-import 'package:actual/restaurant/repository/restaurant_repository.dart';
 import 'package:actual/restaurant/view/restaurant_detail_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -8,24 +6,62 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../common/model/cursor_pagination_model.dart';
 import '../component/restaurant_card.dart';
 
-class RestaurantScreen extends ConsumerWidget {
+class RestaurantScreen extends ConsumerStatefulWidget {
   const RestaurantScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<RestaurantScreen> createState() => _RestaurantScreenState();
+}
+
+class _RestaurantScreenState extends ConsumerState<RestaurantScreen> {
+  final ScrollController controller = ScrollController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller.addListener(scrollListener);
+  }
+
+  void scrollListener() {
+    //현재 위치가
+    //최대 길이보다 조금 덜되는 위치까지 왔다면
+    //새로운 데이터를 추가요청한다.
+
+    //최대스크롤가능한 길이 마이너스 300 보다 현재 스크롤 위치가 크면
+    if (controller.offset > controller.position.maxScrollExtent - 300) {
+      ref.read(restaurantProvider.notifier).paginate(fetchMore: true);
+    }
+    print('run');
+  }
+
+  @override
+  Widget build(
+    BuildContext context,
+  ) {
     final data = ref.watch(restaurantProvider); //계속 생성이 된 이후에 기억이 되므로.
 
-    //로딩일때
-    if (data is CursorPaginationLoading){
+    //완전 처음 로딩일때
+    if (data is CursorPaginationLoading) {
       return Center(
         child: CircularProgressIndicator(),
       );
     }
 
+    if (data is CursorPaginationError) {
+      return Center(
+        child: Text(data.message),
+      );
+    }
+
+    //CursorPagination
+    //CursorPaginationFetchingMore
+    //CursorPaginationRefetching
+
     final cp = data as CursorPagination;
 
-
     return ListView.separated(
+      controller: controller,
       itemCount: cp!.data.length,
       separatorBuilder: (context, index) => SizedBox(
         height: 16.0,
@@ -48,6 +84,5 @@ class RestaurantScreen extends ConsumerWidget {
         );
       },
     );
-
   }
 }
