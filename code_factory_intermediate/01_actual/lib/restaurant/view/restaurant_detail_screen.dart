7d7b745627d0b15bad60cf8221531afb2a67d3f6
isnow890 <isnow890@hotmail.com>
2023-04-30 00:40:50 +0900

    RatingPagination FetchMore 적용하기

--- code_factory_intermediate/01_actual/lib/restaurant/view/restaurant_detail_screen.dart
+++ code_factory_intermediate/01_actual/lib/restaurant/view/restaurant_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:actual/common/layout/default_layout.dart';
+import 'package:actual/common/utils/pagination_utils.dart';
 import 'package:actual/rating/component/rating_card.dart';
 import 'package:actual/restaurant/component/restaurant_card.dart';
 import 'package:actual/restaurant/model/restaurant_detail_model.dart';
@@ -25,11 +26,15 @@ class RestaurantDetailScreen extends ConsumerStatefulWidget {
 
 class _RestaurantDetailScreenState
     extends ConsumerState<RestaurantDetailScreen> {
+  final ScrollController controller = ScrollController();
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     ref.read(restaurantProvider.notifier).getDetail(id: widget.id);
+
+    controller.addListener(scrollListener);
   }
 
   @override
@@ -49,6 +54,7 @@ class _RestaurantDetailScreenState
     return DefaultLayout(
       title: '불타는 떡볶이',
       child: CustomScrollView(
+        controller: controller,
         slivers: [
           renderTop(model: state!),
           if (state is! RestaurantDetailModel) renderLoading(),
@@ -66,13 +72,15 @@ class _RestaurantDetailScreenState
 
   SliverPadding renderRatings({required List<RatingModel> models}) {
     return SliverPadding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0,vertical: 16.0),
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
         sliver: SliverList(
           delegate: SliverChildBuilderDelegate(
               (_, index) => Padding(
-                padding: const EdgeInsets.only(bottom:16.0,),
-                child: RatingCard.fromModel(model: models[index]),
-              ),
+                    padding: const EdgeInsets.only(
+                      bottom: 16.0,
+                    ),
+                    child: RatingCard.fromModel(model: models[index]),
+                  ),
               childCount: models.length),
         ));
   }
@@ -145,6 +153,13 @@ class _RestaurantDetailScreenState
           ),
         ),
       ),
+    );
+  }
+
+  void scrollListener() {
+    PaginationUtils.paginate(
+      controller: controller,
+      provider: ref.read(restaurantRatingProvider(widget.id).notifier),
     );
   }
 }
--- code_factory_intermediate/01_actual/lib/restaurant/view/restaurant_screen.dart
+++ code_factory_intermediate/01_actual/lib/restaurant/view/restaurant_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../common/model/cursor_pagination_model.dart';
+import '../../common/utils/pagination_utils.dart';
 import '../component/restaurant_card.dart';
 
 class RestaurantScreen extends ConsumerStatefulWidget {
@@ -24,15 +25,10 @@ class _RestaurantScreenState extends ConsumerState<RestaurantScreen> {
   }
 
   void scrollListener() {
-    //현재 위치가
-    //최대 길이보다 조금 덜되는 위치까지 왔다면
-    //새로운 데이터를 추가요청한다.
 
-    //최대스크롤가능한 길이 마이너스 300 보다 현재 스크롤 위치가 크면
-    if (controller.offset > controller.position.maxScrollExtent - 300) {
-      ref.read(restaurantProvider.notifier).paginate(fetchMore: true);
-    }
-    print('run');
+    PaginationUtils.paginate(
+        controller: controller,
+        provider: ref.read(restaurantProvider.notifier));
   }
 
   @override
