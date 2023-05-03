924c8cef7db5679e6dcbf365858ff23f9026c863
isnow890 <isnow890@hotmail.com>
2023-05-04 01:11:52 +0900

    존재하지않는 데이터 캐시에 입력하기

--- code_factory_intermediate/01_actual/lib/restaurant/provider/restaurant_provider.dart
+++ code_factory_intermediate/01_actual/lib/restaurant/provider/restaurant_provider.dart
@@ -5,33 +5,32 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../common/model/cursor_pagination_model.dart';
 import '../model/restaurant_model.dart';
+import 'package:collection/collection.dart';
 
 //반환 RestaurantModel, 입력값 String
 final restaurantDetailProvider =
-Provider.family<RestaurantModel?, String>((ref, id) {
+    Provider.family<RestaurantModel?, String>((ref, id) {
   final state = ref.watch(restaurantProvider);
   //데이터가 리스트에 없으므로.
   if (state is! CursorPagination) {
     return null;
   }
 
-  return state.data.firstWhere((element) => element.id == id);
+  return state.data.firstWhereOrNull((element) => element.id == id);
 });
 
 final restaurantProvider =
-StateNotifierProvider<RestaurantStateNotifier, CursorPaginationBase>(
-      (ref) {
+    StateNotifierProvider<RestaurantStateNotifier, CursorPaginationBase>(
+  (ref) {
     final repository = ref.watch(restaurantRepositoryProvider);
     final notifier = RestaurantStateNotifier(repository: repository);
     return notifier;
   },
 );
 
-
-
-class RestaurantStateNotifier extends PaginationProvider<RestaurantModel, RestaurantRepository> {
+class RestaurantStateNotifier
+    extends PaginationProvider<RestaurantModel, RestaurantRepository> {
   RestaurantStateNotifier({required super.repository});
-
 
   void getDetail({
     required String id,
@@ -52,8 +51,37 @@ class RestaurantStateNotifier extends PaginationProvider<RestaurantModel, Restau
 
     //새로 요청한 데이터로 변경함.
 
-    state = pState.copywith(
-      data: pState.data.map<RestaurantModel>((e) => e.id == id ? resp : e).toList(),);
+    //rest1, rest2, rest3
+    //요청 id : 10
+    // list.where((e_=> e.id ==10)) 데이터 X
+    // 데이터가 없을때는 그냥 개시의 끝에다가 데이터를 추가해버린다.
 
+
+
+      // state = pState.copywith(data:<RestaurantModel> [
+      //   ...pState.data,
+      //   //캐시의 끝에 조회한 데이터 추가
+      //   resp,
+      // ]);
+
+    // state = pState.copywith(
+    //   data: pState.data
+    //       .map<RestaurantModel>((e) => e.id == id ? resp : e)
+    //       .toList(),
+    // );
+
+    if (pState.data.where((element) => element.id) == null) {
+      state = pState.copywith(data:<RestaurantModel> [
+        ...pState.data,
+        //캐시의 끝에 조회한 데이터 추가
+        resp,
+      ]);
+    } else {
+      state = pState.copywith(
+        data: pState.data
+            .map<RestaurantModel>((e) => e.id == id ? resp : e)
+            .toList(),
+      );
+    }
   }
 }
