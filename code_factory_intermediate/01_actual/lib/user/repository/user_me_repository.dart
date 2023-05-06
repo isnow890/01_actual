a26d63dc14cca02d2923eb10be625d72f1e84e87
isnow890 <isnow890@hotmail.com>
2023-05-06 23:34:25 +0900

    UserMeRepository 생성하기

---
+++ code_factory_intermediate/01_actual/lib/user/repository/user_me_repository.dart
@@ -0,0 +1,28 @@
+import 'package:actual/common/const/data.dart';
+import 'package:actual/common/dio/dio.dart';
+import 'package:dio/dio.dart' hide Headers;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:retrofit/http.dart';
+
+import '../model/user_model.dart';
+
+part 'user_me_repository.g.dart';
+
+
+final userMeRepositoryProvider = Provider<UserMeRepository>((ref) {
+final dio = ref.watch(dioProvider);
+final repository = UserMeRepository(dio,baseUrl: 'http://$ip/user/me');
+  return repository;
+});
+
+// http://$ip/user/me
+@RestApi()
+abstract class UserMeRepository {
+factory UserMeRepository(Dio dio, {String baseUrl})= _UserMeRepository;
+
+@GET('/')
+  @Headers({'accessToken':'true'})
+  Future<UserModel> getMe();
+
+
+}
