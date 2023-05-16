02fc00c691851cf20d27edc8b4515fd517c1d611
isnow890 <isnow890@hotmail.com>
2023-05-17 00:01:39 +0900

    OrderModel 생성하기

--- code_factory_intermediate/01_actual/lib/common/utils/data_utils.dart
+++ code_factory_intermediate/01_actual/lib/common/utils/data_utils.dart
@@ -3,19 +3,22 @@ import 'dart:convert';
 import '../../restaurant/model/restaurant_model.dart';
 import '../const/data.dart';
 
-class DataUtils{
+class DataUtils {
   static String pathToUrl(String value) => 'http://$ip/${value}';
 
   static findEnum(String value) => RestaurantPriceRange.values
       .firstWhere((element) => element.name == value);
 
-  static List<String> listPathsToUrls(List paths)=> paths.map((e) => pathToUrl(e)).toList();
+  static List<String> listPathsToUrls(List paths) =>
+      paths.map((e) => pathToUrl(e)).toList();
 
-  static String plainToBase64(String plain){
+  static String plainToBase64(String plain) {
     Codec<String, String> stringToBase64 = utf8.fuse(base64);
     String encoded = stringToBase64.encode(plain);
     return encoded;
-
   }
 
+  static DateTime stringToDateTime(String value) {
+    return DateTime.parse(value);
+  }
 }
